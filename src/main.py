d19ebea58069274ca53a391ff8025024688e1b00
Gustavo Barros Dutra Santos <gustavo.dutra11@gmail.com>
2024-01-08 13:15:00 -0300

    refactor: change static paths to input path by client

--- src/main.py
+++ src/main.py
@@ -4,10 +4,15 @@ import time
 
 def main():
 
-    file_path = "C:\\Users\\gusta\\OneDrive\\Área de Trabalho\\dados.xlsx"
-    output_folder = "C:\\Users\\gusta\\OneDrive\\Área de Trabalho\\relatorios"
-    template_text = input("Digite o caminho completo para o template: ")
+    # file_path = "C:\\Users\\gusta\\OneDrive\\Área de Trabalho\\dados.xlsx"
+    file_path = input("Digite o caminho completo para o arquivo de dados: ")
+
+    # output_folder = "C:\\Users\\gusta\\OneDrive\\Área de Trabalho\\relatorios"
+
+    output_folder = input("Digite o caminho completo para a pasta de saída dos relatórios: ")
+    
     # template_text = "C:\\Users\\gusta\\OneDrive\\Documentos\\template.docx"
+    template_text = input("Digite o caminho completo para o template: ")
     
     start_time = time.time()
     data_reader = DataReader(file_path)
