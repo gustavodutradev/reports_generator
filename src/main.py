032ad8bc7e39e86d537c829801a9b9d4ae115c43
Gustavo Barros Dutra Santos <gustavo.dutra11@gmail.com>
2024-01-12 11:31:14 -0300

    refactor: fix module DataHandler usage

--- src/main.py
+++ src/main.py
@@ -1,24 +1,14 @@
-from data_reader import DataReader
+from convert_json import DataHandler
 from report_generator import ReportGenerator
-import sys
 import time
 
 def main(file_path, output_folder, template_text):
     start_time = time.time()
     
-    data_reader = DataReader(file_path)
-    data = data_reader.read_data()
+    data_reader = DataHandler(file_path)
+    data = data_reader.convert_to_json()
 
     report_generator = ReportGenerator(output_folder, template_text, data)
     report_generator.save_report()
 
     print(f"Tempo de execução: {round(time.time() - start_time, 3)} segundos.")
-
-if __name__ == '__main__':
-    try:
-        output_folder = sys.argv[1]
-        template_text = sys.argv[2]
-        file_path = sys.argv[3]
-        main(file_path, output_folder, template_text)
-    except IndexError:
-        print("Argumentos inválidos")
