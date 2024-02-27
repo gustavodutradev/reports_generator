edc8fd9336c87c12c27a538a0d5bca8a008f3699
Gustavo Barros Dutra Santos <gustavo.dutra11@gmail.com>
2024-02-27 16:12:07 -0300

    refactor: implement exception at main.py

--- src/main.py
+++ src/main.py
@@ -2,10 +2,14 @@ from data_loader import DataLoader
 from report_generator import ReportGenerator
 
 def main(file_path, output_folder, template_text):
+    try:
+        data_reader = DataLoader(file_path)
+        data = data_reader.load_dataframe()
 
-    data_reader = DataLoader(file_path)
-    data = data_reader.load_dataframe()
+        report_generator = ReportGenerator(output_folder, template_text, data)
+        report_generator.save_report()
 
-    report_generator = ReportGenerator(output_folder, template_text, data)
-    report_generator.save_report()
-
+        return True
+    except Exception as e:
+        print(f"Erro ao criar relatórios: {e}")
+        return False
