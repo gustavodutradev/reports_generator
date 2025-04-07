dfa64add857843ba7278c330be0817f865215c2b
gustavodutradev <gustavobdsdev@gmail.com>
2025-04-07 14:27:44 -0300

    refactor: main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,33 @@
-from data_loader import DataLoader
-from report_generator import ReportGenerator
+import pandas as pd
+import requests
+from src.report_generator import ReportGenerator
+import dotenv
+import os
 
-def main(file_path, output_folder, template_text):
+dotenv.load_dotenv()
+
+API_URL = os.getenv("API_URL")
+
+
+def main(output_folder, template_text):
     try:
-        data_reader = DataLoader(file_path)
-        data = data_reader.load_dataframe()
+        print("Disparando solicitação para a API BTG...")
+        response = requests.post(API_URL)
 
-        report_generator = ReportGenerator(output_folder, template_text, data)
+        if response.status_code not in [200, 202]:
+            raise Exception(
+                f"Erro ao solicitar dados: {response.status_code} - {response.text}"
+            )
+
+        data = response.json()
+        if "data" not in data:
+            raise Exception("Dados de rentabilidade não encontrados na resposta.")
+
+        df = pd.DataFrame(data["data"])
+        if df.empty:
+            raise ValueError("Nenhum dado retornado pela API.")
+
+        report_generator = ReportGenerator(output_folder, template_text, df)
         report_generator.save_report()
 
         return True
