2dee58a09cb4a1ced73146fb8438e289ccffceb6
gustavodutradev <gustavobdsdev@gmail.com>
2025-04-30 18:05:15 -0300

    delete: test files and old gui

--- src/teste.py
+++
@@ -1,7 +0,0 @@
-from src.local_test_runner import main_teste_local
-
-csv_path = "data/Rentabilidade Mensal Por Carteira.csv"
-template_path = "data/template.docx"
-output_folder = "output_test"
-
-main_teste_local(csv_path, template_path, output_folder)
