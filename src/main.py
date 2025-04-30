1cd8c4def69d6132c82fbe7b67697565c9174b00
gustavodutradev <gustavobdsdev@gmail.com>
2025-04-30 18:07:21 -0300

    refactor: main now make a DB search to get data

--- src/main.py
+++ src/main.py
@@ -1,36 +1,45 @@
-import pandas as pd
-import requests
-from src.report_generator import ReportGenerator
 import dotenv
-import os
+import pandas as pd
+from sqlalchemy import text
+from src.utils.cache_relations import engine
+from src.report_generator import ReportGenerator
 
 dotenv.load_dotenv()
 
-API_URL = os.getenv("API_URL")
+def main(output_folder, template_text, ano, mes):
+    """ Gera relatórios de rentabilidade a partir do banco de dados."""
+    data_ref = f"{ano}-{int(mes):02d}-01"
 
+    # Consulta de rentabilidade mensal
+    query_mtd = text(
+        """
+        SELECT nr_conta, profitability_mtd, cdi_mtd, profit_mtd
+        FROM public.rentabilidade_mensal_por_carteira
+        WHERE mes_referencia = :data_ref
+        """
+    )
+    df_mtd = pd.read_sql_query(query_mtd, engine, params={"data_ref": data_ref})
 
-def main(output_folder, template_text):
-    try:
-        print("Disparando solicitação para a API BTG...")
-        response = requests.post(API_URL)
+    # Consulta de rentabilidade acumulada
+    query_12m = text(
+        """
+        SELECT nr_conta, profitability_12m, cdi_12m, profit_12m
+        FROM public.rentabilidade_acumulada_por_carteira
+        WHERE mes_referencia = :data_ref
+        """
+    )
+    df_ac = pd.read_sql_query(query_12m, engine, params={"data_ref": data_ref})
 
-        if response.status_code not in [200, 202]:
-            raise Exception(
-                f"Erro ao solicitar dados: {response.status_code} - {response.text}"
-            )
-
-        data = response.json()
-        if "data" not in data:
-            raise Exception("Dados de rentabilidade não encontrados na resposta.")
-
-        df = pd.DataFrame(data["data"])
-        if df.empty:
-            raise ValueError("Nenhum dado retornado pela API.")
-
-        report_generator = ReportGenerator(output_folder, template_text, df)
-        report_generator.save_report()
-
-        return True
-    except Exception as e:
-        print(f"Erro ao criar relatórios: {e}")
+    # Validação de dados
+    if df_mtd.empty and df_ac.empty:
+        print(f"Erro: não há dados para {data_ref}")
         return False
+
+    # União dos DataFrames
+    df = df_mtd.merge(df_ac, on="nr_conta", how="outer")
+
+    # Geração dos relatórios
+    report_generator = ReportGenerator(output_folder, template_text, df)
+    report_generator.save_report()
+
+    return True
